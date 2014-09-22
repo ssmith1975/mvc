39cade0872a354ed189d7cb00bca26d2a77c541c
S Smith <ssmith1975.soc@gmail.com>
2014-09-22 00:12:38 -0500

    First commit

---
+++ AcmeBiz/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcmeBiz.BL;
+using AcmeBiz.ViewModel;
+
+namespace AcmeBiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ProductNamesViewModel viewModel = new ProductNamesViewModel()
+            {
+                ProductNameList = ProductsRepository.GetProductNames()
+            };
+            
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ AcmeBiz/ViewModel/ProductNamesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcmeBiz.ViewModel
+{
+    public partial class ProductNamesViewModel
+    {
+        public string ProductName { get; set; }
+        public List<string> ProductNameList { get; set; }
+    }
+}
